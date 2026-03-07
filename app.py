dd0080e0ed82ab2c5b840cd55adc003fe38712a5
vishal ganupa <vishalganupa@gmail.com>
2026-03-07 17:25:59 +0530

    fix deployment

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from typing import Dict
@@ -397,9 +399,6 @@ if __name__ == '__main__':
     logger.info("="*60)
     
     # Run the application
-    port = 5000
-    app.run(
-        host='0.0.0.0',
-        port=port,
-        debug=Config.DEBUG
-    )
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
